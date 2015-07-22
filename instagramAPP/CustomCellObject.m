4dae4c25f787ec7e879e82583a88be7ed3360739
mxnmike <mxnmike@me.com>
2015-07-21 23:01:17 -0400

    2nd Submit

--- instagramAPP/CustomCellObject.m
+++ instagramAPP/CustomCellObject.m
@@ -2,7 +2,7 @@
 //  CustomCellObject.m
 //  instagramAPP
 //
-//  Created by Miguel Garcia Topete on 7/21/15.
+//  Created by Mobile Apps Company on 7/21/15.
 //  Copyright (c) 2015 MAC. All rights reserved.
 //
 
